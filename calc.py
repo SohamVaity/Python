2eb7d981ddc481174a26d8a66b277a552316b0b7
Soham Vaity <137893804+SohamVaity@users.noreply.github.com>
2023-07-10 20:56:57 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,36 @@
+class Calculator:
+    def calculate():
+        n1=1
+        while n1 == 1:
+            print("\n This is menu driven program")
+            print("1) Addition \n2) Subtraction \n3) Multiplication \n4) Division \n")
+            num=int(input("Enter your choice: "))
+            if num == 1:
+                x=float(input("Enter first value :"))
+                y=float(input("Enter second value :"))
+                print("Addition is : ",(x+y))
+            elif num==2:
+                x=float(input("Enter first value :"))
+                y=float(input("Enter second value :"))
+                print("Subtraction is : ",(x-y))
+            elif num==3 :
+                x=float(input("Enter first value :"))
+                y=float(input("Enter second value :"))
+                print("Multiplication is : ",(x*y))
+            elif num==4:
+                x=float(input("Enter first value :"))
+                y=float(input("Enter second value :"))
+                print("Division is : ",(x/y))
+            else:
+                print("Wrong choice")
+            n=input("If you want to continue, press 1 \n press any key to exit ")
+            if n!="1":
+                exit()
+            n1=int(n)
+            
+    
+c=Calculator
+c.calculate()
+
+#Soham Vaity batch 3
+
